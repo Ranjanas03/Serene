24dfc7d53438d8807ffde9dd389d085b00a640c4
Ranjanas03 <115175126+Ranjanas03@users.noreply.github.com>
2024-08-09 14:52:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+var tl = gsap.timeline()
+tl.to("#main",{
+    y:"500vh",
+    scale:0.6,
+    duration:0
+})
+tl.to("#main",{
+    y:"30vh",
+    duration:1,
+    delay:1
+})
+tl.to("#main",{
+    y:"0vh",
+    rotate:360,
+    scale:1,
+    duration:0.8
+})
